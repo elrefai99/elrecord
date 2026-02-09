e1f496edaa47903a2d06abfb24cb3dcf843458f8
Mohamed El-Refai <elrefai99@gmail.com>
2026-02-09 13:43:46 +0200

    feat: Implement friend request functionality including a check for existing friendships.

--- src/Module/friends/controller/friends.request.controller.ts
+++ src/Module/friends/controller/friends.request.controller.ts
@@ -1,8 +1,27 @@
 import { NextFunction, Request, Response } from "express";
 import { asyncHandler } from "../../../utils/asyncHandler.utils";
+import { FriendsService } from "../friends.service";
+import ServerError from "../../../utils/api.errors.utils";
+import { v4 as uuidv4 } from 'uuid';
 
 export const requestFriendController = asyncHandler(
      async (req: Request, res: Response, next: NextFunction) => {
+          const payload = req.body
+          const friendService: FriendsService = new FriendsService()
+          const check_friendship = await friendService.check_friendship(req.user.id as number)
 
+          if (check_friendship) {
+               next(new ServerError("You are already friends", 400))
+               return
+          }
+          const uuid = uuidv4()
+          await friendService.create_friend_request({
+               userId: req.user.id as number,
+               receiverId: payload.receiverId as number,
+               uuid: uuid,
+               status: "PENDING",
+          })
+
+          return res.status(200).json({ code: 200, status: "OK", timestamp: new Date().toISOString(), message: "Friend request sent successfully", })
      }
 )
